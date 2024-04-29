f428b5ee85b004923bb157105a624840b72b201b
CarlosBB06 <carlosballara03@gmail.com>
2024-04-29 20:01:17 +0200

    .

---
+++ js/comprar.js
@@ -0,0 +1,10 @@
+var apikey = (function() {
+    var apikey = "38f6fe89b061754d4a174474fd82af2a8c6ba01758f0c28c4309ec53c8588696";
+    
+    return {
+        obtenerApiKey: function() {
+            return apikey;
+        }
+    };
+})();
+
